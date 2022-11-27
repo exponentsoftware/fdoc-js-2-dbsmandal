094a912f28e16c0eb3632719cb1afdc37d5e1db6
dbsmandal <dipakmandaljio420@gmail.com>
2022-11-28 01:20:54 +0530

    Second JavaScript Agignment Completed

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const countries = [
 
 
 const createArrayOfArrays = (countries) => {
-	// console.log("rest", countries)
 	let finalArray = []
 	for (var i = 0; i < countries.length; i++) {
 		var myArray = countries[i][0] + countries[i].substring(1).toLowerCase();
@@ -24,11 +23,12 @@ const createArrayOfArrays = (countries) => {
 		// var array = mainArray
 		// console.log("Capitalize", myArray,firstThreeLetter,contryLength)
 		// console.log("mainArray", mainArray)
-		console.log("arrayyyyyyyyyyy", subArray)
+		// console.log("arrayyyyyyyyyyy", subArray)
+		finalArray.push(subArray)
 	}
-	return subArray
+	return finalArray
 }
-console.log("lastaaaaaaaa", createArrayOfArrays(countries))
+console.log("Final Array ", createArrayOfArrays(countries))
 
 const users = [
 	{
@@ -94,19 +94,20 @@ function scoresGreaterThan85Normal(users) {
 
 console.log("scoresGreaterThan85 normal ", scoresGreaterThan85Normal(users))
 
-const obj1 = {
-	name: 'Dipak', scores: 90,
+const newUser1 = {
+	name: 'Dipak',
+	scores: 90,
 	skills: ['HTM', 'CSS', 'JS'],
 	age: 25
 };
 
-const addUser = (users, obj1) => {
+const addUser = (users, newUser) => {
 	const arr = [];
-	arr.push(obj1)
+	arr.push(newUser)
 	let newArray = users.concat(arr)
 	return newArray
 }
-console.log("newuser new  list", addUser(users, obj1))
+console.log("newuser new  list", addUser(users, newUser1))
 let obj2 = {
 	name: 'Goutam',
 	scores: 90,
@@ -115,3 +116,97 @@ let obj2 = {
 }
 
 console.log("newuser second  list", addUser(users, obj2))
+
+//add new user solution
+
+
+let newUser2 = {
+	name: 'Goutam',
+	scores: 90,
+	skills: ['HTM', 'CSS', 'JS'],
+	age: 20
+}
+
+let newUser3 = {
+	name: 'Thomas',
+	scores: 90,
+	skills: ['HTM', 'CSS', 'JS'],
+	age: 20
+}
+let newUser4 = {
+	name: 'Dipak',
+	scores: 90,
+	skills: ['HTM', 'CSS', 'JS'],
+	age: 20
+}
+
+const addNewUser=(arr,newUser)=>{
+    for(const user of arr){
+        if(user['name']===newUser.name){
+            return "A user already exist";
+        }
+    }
+     arr.push(newUser)
+    
+    return arr;
+}
+
+
+console.log("newuser Sampa  list", addNewUser(users, newUser1))
+console.log("newuser Goutam  list", addNewUser(users, newUser2))
+console.log("newuser Thomas  list", addNewUser(users, newUser3))
+console.log("newuser Dipak  list", addNewUser(users, newUser4))
+console.log("newuser second time Dipak  list", addNewUser(users, newUser4))
+//add user skill
+/////////////////////////////////////
+const addUserSkill = (arr, name, skill) => {
+	let found = false;
+	for (const user of arr) {
+		if (user['name'] === name) {
+			user.skills.push(skill);
+			found = true;
+			break
+		}
+		
+	}
+	if (!found) {
+		return 'A user does not exist';
+	}
+
+	return arr
+}
+console.log("add user newskills", addUserSkill(users, 'Thomas', 'React Developer'))
+
+const editUser = (arr, name, newUser) => {
+	let found = false;
+	for (let user of arr) {
+		if (user['name'] === name) {
+			user.name = newUser.name;
+			user.scores = newUser.scores;
+			user.skills = newUser.skills;
+			user.age = newUser.age;
+			found = true;
+			break
+		}
+	}
+	if (!found) {
+		return "A user Does not Exist"
+	}
+	return arr;
+}
+
+const newUser = {
+	name: 'Thomas',
+	scores: 75,
+	skills: ['HTM', 'CSS', 'JS', 'React', 'bootstrap'],
+	age: 24
+}
+
+console.log("edituser Thomas", editUser(users, 'Thomas', newUser))
+
+console.log("lastaaaaaaaa", createArrayOfArrays(countries))
+
+
+const createArrayOfArraysNew=()=>{
+
+}
