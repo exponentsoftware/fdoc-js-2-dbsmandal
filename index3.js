e6fc8b8ca28f45944a8d2302ecbdb2033137e726
dbsmandal <dipakmandaljio420@gmail.com>
2022-11-27 23:20:59 +0530

    Add NewSkills Problem Solved

---
+++ index3.js
@@ -0,0 +1,61 @@
+const users = [
+    {
+        name: 'Brook',
+        scores: 75,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 16
+    },
+    {
+        name: 'Alex',
+        scores: 80,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 18
+    },
+    {
+        name: 'David',
+        scores: 75,
+        skills: ['HTM', 'CSS'],
+        age: 22
+    },
+    {
+        name: 'John',
+        scores: 85,
+        skills: ['HTM'],
+        age: 25
+    },
+    {
+        name: 'Sara',
+        scores: 95,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name: 'Martha',
+        scores: 80,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 18
+    },
+    {
+        name: 'Thomas',
+        scores: 90,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 20
+    }
+];
+const addUserSkill = (arr, name, skill) => {
+    let found = false;
+    for (const user of arr) {
+      if (user['name'] === name) {
+        user.skills.push(skill);
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      return 'A user does not exist';
+    }
+  
+    return arr;
+  };
+  console.log(addUserSkill(users, 'Brook', 'Node'));
+  
